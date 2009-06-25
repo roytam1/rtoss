726f96c86c3ec8e925c054a8df677c7999109384
roytam <roytam@d585c2e8-173e-3ebf-1cc4-9d1291936ed3>
2009-06-25 13:11:16 +0000

    - [cndos-up] multiple delete, move settings to settings.php, statement optimizing

--- cndos-up/upload.php
+++ cndos-up/upload.php
@@ -23,61 +23,33 @@
   -------------------------------------------
  *********************************************/
 error_reporting(0);
-if(phpversion()>="4.1.0"){//PHP4.1.0以降対応
-  $_GET = array_map("_clean", $_GET);
-  $_POST = array_map("_clean", $_POST);
-  //$_POST = array_map("_clean", $_GET);
+if(phpversion()>='4.1.0'){//PHP4.1.0以降対応
+  $_GET = array_map('_clean', $_GET);
+  $_POST = array_map('_clean', $_POST);
+  //$_POST = array_map('_clean', $_GET);
   extract($_GET);
   extract($_POST);
   extract($_COOKIE);
   extract($_SERVER);
   $upfile_type=_clean($_FILES['upfile']['type']);
-  $upfile_size=$_FILES["upfile"]["size"];//某所で気づく・・・
-  $upfile_name=_clean($_FILES["upfile"]["name"]);
-  $upfile=$_FILES["upfile"]["tmp_name"];
+  $upfile_size=$_FILES['upfile']['size'];//某所で気づく・・・
+  $upfile_name=_clean($_FILES['upfile']['name']);
+  $upfile=$_FILES['upfile']['tmp_name'];
 }
 
-  $title	= '上傳區';	//タイトル
-  $home		= 'http://www.cn-dos.net/';	//ホーム
-  $logfile	= "up.log";	//ログファイル名（変更する事）
-  $updir 	= "./img/";	//アップ用ディレクトリ
-  $upsdir	= "./imgs/";	//サムネイル保存ディレクトリ
-  $prefix	= '';		//接頭語（up001.txt,up002.jpgならup）
-  $logmax	= 5000;		//log保存行（これ以上は古いのから削除）
-  $usrmax	= 25;		//User投稿量制限（バイト。全角はこの半分）
-  $commax	= 250;		//コメント投稿量制限（バイト。全角はこの半分）
-  $limitk	= 100000;	//アップロード制限（KB キロバイト）
-  $page_def	= 15;		//一ページの表示行数
-  $admin	= "del";	//削除管理パス
-  $auto_link	= 1;		//コメントの自動リンク（Yes=1;No=0);
-  $denylist	= array('192.168.0.1','sex.com','annony');	//アクセス拒否ホスト
-  $arrowext	= array('7z','zip','lzh','rar','avi','mpg','wmv','mp3','wma','swf','txt','bmp','jpg','png','gif');	//許可拡張子 小文字（それ以外はエラー
+include_once('./settings.php');
 
-  $count_start	= "2008/02/11";	//カウンタ開始日
-  $count_file	= "count.txt";  //カウンタファイル（空ファイルで666）
-
-  $last_file	= "last.cgi";	//連続投稿制限用ファイル（空ファイルで666）
-  $last_time	= 0;		//同一IPからの連続投稿許可する間隔（分）（0で無制限）
-
-  /* 項目表示（環境設定）の初期状態 (表示ならChecked 表示しないなら空) */
-  $f_act  = 'checked="checked"';	//ACT（削除リンク）
-  $f_usr  = 'checked="checked"';	//コメント
-  $f_com  = 'checked="checked"';	//コメント
-  $f_size = 'checked="checked"';	//ファイルサイズ
-  $f_mime = '';				//MIMEタイプ
-  $f_date = 'checked="checked"';	//日付け
-  $f_orig = 'checked="checked"';	//元ファイル名
-
-if($act=="envset"){
-  $cookval = @implode(",", array($acte,$user,$come,$sizee,$mimee,$datee,$orige)); 
-  setcookie ("upcook", $cookval,time()+365*24*3600);
+if($act=='envset'){
+  $cookval = @implode(',', array($acte,$user,$come,$sizee,$mimee,$datee,$orige)); 
+  setcookie('upcook', $cookval,time()+365*24*3600);
 }
 function _clean($str) {
+  if(is_array($str)) return $str;
   $str = htmlspecialchars($str);
   if (get_magic_quotes_gpc()) $str = stripslashes($str);
   return $str;
 }
-$unique_id = uniqid("");
+$unique_id = uniqid('');
 /* ここからヘッダー */
 ?>
 <?php echo '<?xml version="1.0" encoding="utf-8"?>'."\n" ?>
@@ -141,7 +113,7 @@ function startProgress(){
 </head>
 <body>
 
-<h1><?php echo "$title" ?></h1>
+<h1><?php echo $title; ?></h1>
 <p>請勿在論壇外轉載此區任何資料。</p>
 <?php
 /* ヘッダーここまで */
@@ -155,27 +127,26 @@ $foot = <<<FOOT
 FOOT;
 
 function FormatByte($size){//バイトのフォーマット（B→kB）
-  if($size == 0)			$format = "";
-  else if($size <= 1024)		$format = $size."B";
-  else if($size <= (1024*1024))		$format = sprintf ("%dkB",($size/1024));
-  else if($size <= (1024*1024*1024))	$format = sprintf ("%.2fMB",($size/(1024*1024)));
-  else if($size <= (1024*1024*1024*1024))	$format = sprintf ("%.2fGB",($size/(1024*1024*1024)));
-  else					$format = $size."B";
-
-  return $format;
+	$suffix=''; $suxAry=array('KB','MB','GB','TB');
+	$ccnt=count($suxAry);
+	for($cnt=0;$cnt<$ccnt;$cnt++)
+		if($size > 1024) {$size/=1024; $suffix=$suxAry[$cnt];}
+	return $suffix?sprintf("%.1f",$size).$suffix:$size.'B';
 }
 function paging($page, $total){//ページリンク作成
   global $PHP_SELF,$page_def;
+
+  $act=isset($_GET['act'])?'&act='.$_GET['act']:'';
 
     for ($j = 1; $j * $page_def < $total+$page_def; $j++) {
       if($page == $j){//今表示しているのはﾘﾝｸしない
         $next .= "[<strong>$j</strong>]";
       }else{
-        $next .= sprintf("[<a href=\"%s?page=%d\">%d</a>]", $PHP_SELF,$j,$j);//他はﾘﾝｸ
+        $next .= sprintf('[<a href="%s?page=%d%s">%d</a>]', $PHP_SELF,$j,$act,$j);//他はﾘﾝｸ
       }
     }
-    if($page=="all") return sprintf ("Page: %s [ALL]",$next,$PHP_SELF);
-    else return sprintf ("Page: %s [<a href=\"%s?page=all\">ALL</a>]",$next,$PHP_SELF);
+    if($page=="all") return sprintf('Page: %s [ALL]',$next);
+    else return sprintf ('Page: %s [<a href="%s?page=all%s">ALL</a>]',$next,$PHP_SELF,$act);
 }
 function error($mes1=""){//えっらーﾒｯｾｰｼﾞ
   global $foot;
@@ -188,7 +159,7 @@ function error($mes1=""){//えっらーﾒｯｾｰｼﾞ
 $limitb = $limitk * 1024;
 $host = @gethostbyaddr($REMOTE_ADDR);
 if(!$upcook) $upcook=@implode(",",array($f_act,$f_usr,$f_com,$f_size,$f_mime,$f_date,$f_orig));
-list($c_act,$c_usr,$c_com,$c_size,$c_mime,$c_date,$c_orig)=explode(",",$upcook);
+list($c_act,$c_usr,$c_com,$c_size,$c_mime,$c_date,$c_orig)=explode(',',$upcook);
 
 /* アクセス制限 */
 if(is_array($denylist)){
@@ -198,7 +169,7 @@ if(is_array($denylist)){
   }
 }
 /* 削除実行 */
-if($delid && $delpass!=""){
+if($delid && $delpass!=''){
   $old = file($logfile);
   $find = false;
   for($i=0; $i<count($old); $i++){
@@ -214,9 +185,9 @@ if($delid && $delpass!=""){
   if(!$find) error('<h2>錯誤</h2>
 <p class="error">刪除錯誤:此檔案找不到</p>');
   if($delpass == $admin || substr(md5($delpass), 2, 7) == $del_pwd){
-    if(file_exists($updir.$prefix.$delid.".$del_ext")) unlink($updir.$prefix.$delid.".$del_ext");
-    if(file_exists($upsdir.$prefix.$delid.".$del_ext")) unlink($upsdir.$prefix.$delid.".$del_ext");
-    $fp = fopen($logfile, "w");
+    if(file_exists($updir.$prefix.$delid.'.'.$del_ext)) unlink($updir.$prefix.$delid.'.'.$del_ext);
+    if(file_exists($upsdir.$prefix.$delid.'.'.$del_ext)) unlink($upsdir.$prefix.$delid.'.'.$del_ext);
+    $fp = fopen($logfile, 'w');
     flock($fp, 2);
     fputs($fp, @implode("",$new));  
     fclose($fp);
@@ -239,7 +210,7 @@ if($del){
 }
 /* 環境設定フォーム */
 if($act=="env"){
-  echo "<h2>環境設定</h2>
+  error("<h2>環境設定</h2>
 <form method=\"get\" action=\"$PHP_SELF\">
 <p><input type=\"hidden\" name=\"act\" value=\"envset\" /></p>
 <h3>顯示設定</h3>
@@ -255,11 +226,48 @@ if($act=="env"){
 <p>以上設定將會以 cookie 保存以便再次使用。</p>
 <p><input type=\"submit\" value=\"儲存\" tabindex=\"8\" accesskey=\"8\" /><input type=\"reset\" value=\"還原\" tabindex=\"9\" accesskey=\"9\" /></p>
 </form>
-<p class=\"tline\"><a href=\"$PHP_SELF?\">返回</a></p>
-
-";
-echo $foot;
-exit;
+<p class=\"tline\"><a href=\"$PHP_SELF?\">返回</a></p>");
+}
+elseif($act=='mdel') {
+	if(!isset($_POST['mdid'])) error('<h2>錯誤</h2>
+<p class="error">刪除錯誤:未選擇檔案</p>');
+	if($delpass==$admin) {
+		$mdid = array_merge(array('dummy'), $_POST['mdid']);
+		$old = file($logfile);
+		$find = false;
+		for($i=0; $i<count($old); $i++){
+			list($did,$dext,)=explode("\t",$old[$i]);
+				if(array_search($did,$mdid)){
+					$find = true;
+					if(file_exists($updir.$prefix.$did.'.'.$dext)) unlink($updir.$prefix.$did.'.'.$dext);
+					if(file_exists($upsdir.$prefix.$did.'.'.$dext)) unlink($upsdir.$prefix.$did.'.'.$dext);
+				}else{
+					$new[] = $old[$i];
+				}
+		}
+	} else {
+		$mdid = array_merge(array('dummy'), $_POST['mdid']);
+		$old = file($logfile);
+		$find = false;
+		for($i=0; $i<count($old); $i++){
+			list($did,$dext,,,,,,,$dpwd,)=explode("\t",$old[$i]);
+				if(array_search($did,$mdid)){
+					if(substr(md5($delpass), 2, 7) == rtrim($dpwd)){
+						$find = true;
+						if(file_exists($updir.$prefix.$did.'.'.$dext)) unlink($updir.$prefix.$did.'.'.$dext);
+						if(file_exists($upsdir.$prefix.$did.'.'.$dext)) unlink($upsdir.$prefix.$did.'.'.$dext);
+					}
+				}else{
+					$new[] = $old[$i];
+				}
+		}
+	}
+	if(!$find) error('<h2>錯誤</h2>
+<p class="error">刪除錯誤:密碼錯誤</p>');
+    $fp = fopen($logfile, 'w');
+    flock($fp, 2);
+    fputs($fp, @implode("",$new));  
+    fclose($fp);
 }
 $lines = file($logfile);
 /* アプロード書き込み処理 */
@@ -275,7 +283,7 @@ if(file_exists($upfile) && $com && $usr && $upfile_size > 0){
   /* 連続投稿制限 */
   if($last_time > 0){
     $now = time();
-    $last = @fopen($last_file, "r+") or die('<h2>錯誤</h2>
+    $last = @fopen($last_file, 'r+') or die('<h2>錯誤</h2>
 <p class="error">連続投稿用ファイル $last_file を作成してください</p>');
     $lsize = fgets($last, 1024);
     list($ltime, $lip) = explode("\t", $lsize);
@@ -288,7 +296,7 @@ if(file_exists($upfile) && $com && $usr && $upfile_size > 0){
     fclose($last);
   }
   /* 拡張子と新ファイル名 */
-  $pos = strrpos($upfile_name,".");	//拡張子取得
+  $pos = strrpos($upfile_name,'.');	//拡張子取得
   $ext = substr($upfile_name,$pos+1,strlen($upfile_name)-$pos);
   $ext = strtolower($ext);//小文字化
   if(!in_array($ext, $arrowext))
@@ -300,8 +308,8 @@ if(file_exists($upfile) && $com && $usr && $upfile_size > 0){
   }
   */
   list($id,) = explode("\t", $lines[0]);//No取得
-  $id = sprintf("%03d", ++$id);		//インクリ
-  $newname = $prefix.$id.".".$ext;
+  $id = sprintf('%03d', ++$id);		//インクリ
+  $newname = $prefix.$id.'.'.$ext;
 
   /* 自鯖転送 */
   move_uploaded_file($upfile, $updir.$newname);//3.0.16より後のバージョンのPHP 3または 4.0.2 後
@@ -309,7 +317,7 @@ if(file_exists($upfile) && $com && $usr && $upfile_size > 0){
   chmod($updir.$newname, 0604);
 
   /* MIMEタイプ */
-  if(!$upfile_type) $upfile_type = "text/plain";//デフォMIMEはtext/plain
+  if(!$upfile_type) $upfile_type = 'text/plain';//デフォMIMEはtext/plain
 
   /* コメント他 */
   $com = htmlspecialchars($com);	//タグ変換
@@ -317,21 +325,21 @@ if(file_exists($upfile) && $com && $usr && $upfile_size > 0){
   $usr = htmlspecialchars($usr);	//タグ変換
   if(get_magic_quotes_gpc()) $usr = stripslashes($usr);	//￥除去
 
-  $now = gmdate("Y/m/d(D)H:i", time()+9*60*60);	//日付のフォーマット
-  $pwd = ($pass) ? substr(md5($pass), 2, 7) : "*";	//パスっ作成（無いなら*）
+  $now = gmdate('Y/m/d(D)H:i', time()+9*60*60);	//日付のフォーマット
+  $pwd = ($pass) ? substr(md5($pass), 2, 7) : '*';	//パスっ作成（無いなら*）
 
   $dat = @implode("\t", array($id,$ext,$usr,$com,$host,$now,$upfile_size,$upfile_type,$pwd,$upfile_name,));
 
   if(count($lines) >= $logmax){		//ログオーバーならデータ削除
     for($d = count($lines)-1; $d >= $logmax-1; $d--){
       list($did,$dext,)=explode("\t", $lines[$d]);
-      if(file_exists($updir.$prefix.$did.".".$dext)) {
-        unlink($updir.$prefix.$did.".".$dext);
+      if(file_exists($updir.$prefix.$did.'.'.$dext)) {
+        unlink($updir.$prefix.$did.'.'.$dext);
       }
     }
   }
 
-  $fp = fopen ($logfile , "w");		//書き込みモードでオープン
+  $fp = fopen ($logfile , 'w');		//書き込みモードでオープン
   flock($fp ,2);
   fputs ($fp, "$dat\n");		//先頭に書き込む
   for($i = 0; $i < $logmax-1; $i++)	//いままでの分を追記
@@ -362,7 +370,7 @@ echo '<h2>上傳檔案</h2>
 /* カウンタ */
 echo "<p class=\"uline\">$count_start 開始存取數: ";
 if(file_exists($count_file)){
-  $fp = fopen($count_file,"r+");//読み書きモードでオープン
+  $fp = fopen($count_file,'r+');//読み書きモードでオープン
   $count = fgets($fp, 64);	//64バイトorEOFまで取得、カウントアップ
   $count++;
   fseek($fp, 0);			//ポインタを先頭に、ロックして書き込み
@@ -374,43 +382,47 @@ if(file_exists($count_file)){
 /* モードリンク */
 echo '
 　D：刪除檔案</p>
-<p class="uline"><a href="'.$home.'">主頁</a> | <a href="'.$PHP_SELF.'?act=env">環境設定</a> | <a href="'.$PHP_SELF.'?">重新整理</a> | <a href="sam.php">圖像一覧</a></p>
+<p class="uline"><a href="'.$home.'">主頁</a> | <a href="'.$PHP_SELF.'?act=mult">多檔刪除</a> | <a href="'.$PHP_SELF.'?act=env">環境設定</a> | <a href="'.$PHP_SELF.'?">重新整理</a> | <a href="sam.php">圖像一覧</a></p>
 
 <h2>檔案一覧</h2>
 <p class="uline">';
 /* ログ開始位置 */
 $st = ($page) ? ($page - 1) * $page_def : 0;
 if(!$page) $page = 1;
-if($page == "all"){
+if($page == 'all'){
   $st = 0;
   $page_def = count($lines);
 }
 echo paging($page, count($lines));//ページリンク
 //メインヘッダ
 echo "</p>\n";
+if($act=='mult')  echo "<form action=\"$PHP_SELF\" method=POST><input type='hidden' name='act' value='mdel'/>";
 echo "<table summary=\"files\">\n<tr>";
-if($c_act) echo "<th abbr=\"delete\" scope=\"col\">刪</th>";
-echo "<th abbr=\"name\" scope=\"col\">檔名</th>";
-if($c_usr) echo "<th abbr=\"user\" scope=\"col\">用戶</th>";
-if($c_com) echo "<th abbr=\"comment\" scope=\"col\">備註</th>";
-if($c_size) echo "<th abbr=\"size\" scope=\"col\">大小</th>";
-if($c_mime) echo "<th abbr=\"mime\" scope=\"col\">MIME</th>";
-if($c_date) echo "<th abbr=\"date\" scope=\"col\">日期</th>";
-if($c_orig) echo "<th abbr=\"original name\" scope=\"col\">原檔名</th>";
+if($c_act) echo '<th abbr="delete" scope="col">刪</th>';
+echo '<th abbr="name" scope="col">檔名</th>';
+if($c_usr) echo '<th abbr="user" scope="col">用戶</th>';
+if($c_com) echo '<th abbr="comment" scope="col">備註</th>';
+if($c_size) echo '<th abbr="size" scope="col">大小</th>';
+if($c_mime) echo '<th abbr="mime" scope="col">MIME</th>';
+if($c_date) echo '<th abbr="date" scope="col">日期</th>';
+if($c_orig) echo '<th abbr="original name" scope="col">原檔名</th>';
 echo "</tr>\n";
 //メイン表示
 for($i = $st; $i < $st+$page_def; $i++){
-  if($lines[$i]=="") continue;
+  if($lines[$i]=='') continue;
   list($id,$ext,$usr,$com,$host,$now,$size,$mtype,$pas,$orig,)=explode("\t",$lines[$i]);
   $fsize = FormatByte($size);
-  if($auto_link) $com = ereg_replace("(https?|ftp|news)(://[[:alnum:]\+\$\;\?\.%,!#~*/:@&=_-]+)","<a href=\"\\1\\2\">\\1\\2</a>",$com);
+  if($auto_link) $com = preg_replace('/(https?|ftp|news)(:\/\/[\w\+\$\;\?\.\{\}%,!#~*\/:@&=_-]+)/u', '<a href="$1$2">$1$2</a>',$com);
 
-  $filename = $prefix.$id.".$ext";
+  $filename = $prefix.$id.'.'.$ext;
   $target = $updir.$filename;
 
   
   echo "<tr><!--$host-->\n";//ホスト表示
-  if($c_act) echo "<td class=\"del\"><a href=\"$PHP_SELF?del=$id\">D</a></td>";
+  if($c_act) {
+  	if($act=='mult') echo "<td class=\"del\"><input type=\"checkbox\" name=\"mdid[]\" value=\"$id\"/></td>";
+  	else echo "<td class=\"del\"><a href=\"$PHP_SELF?del=$id\">D</a></td>";
+  }
   echo "<td>[<a href=\"$target\">$filename</a>]</td>";
   if($c_usr) echo "<td>$usr</td>";
   if($c_com) echo "<td>$com</td>";
@@ -421,9 +433,9 @@ for($i = $st; $i < $st+$page_def; $i++){
   echo "</tr>\n";
   }
 
-echo "</table>\n<p class=\"tline\">";
-echo paging($page,count($lines));
-echo "</p>\n";
+echo "</table>\n";
+if($act=='mult')  echo '<div align="right">密碼:<input type="password" name="delpass"/><input type="submit" name="submit" value="刪除"/></div></form>';
+echo '<p class="tline">'.paging($page,count($lines))."</p>\n";
 echo $foot;
 ?>
 
