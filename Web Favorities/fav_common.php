ffaf808ae63a4edf7abfcd18401eaf911f977367
roytam <roytam@d585c2e8-173e-3ebf-1cc4-9d1291936ed3>
2009-08-18 12:03:52 +0000

    - [Web Favorities] * Make bookmarklet work with non-Unicode pages

--- Web Favorities/fav_common.php
+++ Web Favorities/fav_common.php
@@ -111,4 +111,33 @@ function viewAuth($action='auth',$pass='') {
 			setcookie("favsess", '', time()-3600);
 	}
 }
+
+function u2utf8($c) {
+	$str = "";$ret='';
+	if ($c < 0x80) {
+		$str .= $c;
+	} else if ($c < 0x800) {
+		$str .= (0xC0 | $c >> 6);
+		$str .= (0x80 | $c & 0x3F);
+	} else if ($c < 0x10000) {
+		$str .= (0xE0 | $c >> 12);
+		$str .= (0x80 | $c >> 6 & 0x3F);
+		$str .= (0x80 | $c & 0x3F);
+	} else if ($c < 0x200000) {
+		$str .= (0xF0 | $c >> 18);
+		$str .= (0x80 | $c >> 12 & 0x3F);
+		$str .= (0x80 | $c >> 6 & 0x3F);
+		$str .= (0x80 | $c & 0x3F);
+	}
+	for ($i = 0;$i < strlen($str);$i += 3) $ret .= chr(substr($str, $i, 3));
+	return $ret;
+}
+
+function jsUCEsc2utf8($str) {
+	preg_match_all('/%u([0-9A-Fa-f]{4})/',$str,$m);
+	foreach($m[1] as $uc) {
+		$str = str_replace("%u$uc",u2utf8(hexdec($uc)),$str);
+	}
+	return $str;
+}
 ?>
