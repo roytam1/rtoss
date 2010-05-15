0b9c49c0a66dd1d4902ca1766efdf4c61147a0e9
roytam <roytam@d585c2e8-173e-3ebf-1cc4-9d1291936ed3>
2010-05-15 16:25:51 +0000

    - [GreenPad] fix forcing use old version of menu API (introduced in previous revision)

--- GreenPad/ConfigManager.cpp
+++ GreenPad/ConfigManager.cpp
@@ -825,7 +825,8 @@ void ConfigManager::SetUpMRUMenu( HMENU m, UINT id )
 	while( ::DeleteMenu( m, 0, MF_BYPOSITION ) );
 
 	// メニュー構築
-	/*if(app().isNewShell())
+#if !defined(TARGET_VER) || (defined(TARGET_VER) && TARGET_VER>310)
+	if(app().isNewShell())
 	{
 		MENUITEMINFO mi = { sizeof(MENUITEMINFO) };
 		mi.fMask = MIIM_ID | MIIM_TYPE;
@@ -853,7 +854,8 @@ void ConfigManager::SetUpMRUMenu( HMENU m, UINT id )
 		}
 	}
 	else
-	{*/
+#endif
+	{
 		for( int i=0; i<countof(mru_); ++i )
 		{
 			if( i>=mrus_ || mru_[i].len()==0 )
@@ -867,7 +869,7 @@ void ConfigManager::SetUpMRUMenu( HMENU m, UINT id )
 			String cpt = mru_[i].CompactIfPossible(60);
 			::InsertMenu( m, i, MF_BYPOSITION, id + i, const_cast<TCHAR*>(cpt.c_str()) );
 		}
-	//}
+	}
 }
 
 Path ConfigManager::GetMRU( int no ) const
@@ -909,7 +911,8 @@ void ConfigManager::SetDocTypeMenu( HMENU m, UINT idstart )
 	// 全項目を削除
 	while( ::DeleteMenu( m, 0, MF_BYPOSITION ) );
 
-	/*if(app().isNewShell())
+#if !defined(TARGET_VER) || (defined(TARGET_VER) && TARGET_VER>310)
+	if(app().isNewShell())
 	{
 		// 順に追加
 		MENUITEMINFO mi = { sizeof(MENUITEMINFO) };
@@ -927,18 +930,20 @@ void ConfigManager::SetDocTypeMenu( HMENU m, UINT idstart )
 		}
 	}
 	else
-	{*/
+#endif
+	{
 		DtList::iterator i=dtList_.begin(), e=dtList_.end();
 		for( int ct=0; i!=e; ++i, ++ct )
 		{
 			::InsertMenu( m, ct, MF_BYPOSITION|(i==curDt_ ? MFS_CHECKED : MFS_UNCHECKED), idstart + ct, const_cast<TCHAR*>(i->name.c_str()) );
 		}
-	//}
+	}
 }
 
 void ConfigManager::SetDocTypeByMenu( int pos, HMENU m )
 {
-	/*if(app().isNewShell())
+#if !defined(TARGET_VER) || (defined(TARGET_VER) && TARGET_VER>310)
+	if(app().isNewShell())
 	{
 		MENUITEMINFO mi = { sizeof(MENUITEMINFO) };
 		mi.fMask  = MIIM_STATE;
@@ -956,7 +961,8 @@ void ConfigManager::SetDocTypeByMenu( int pos, HMENU m )
 		}
 	}
 	else
-	{*/
+#endif
+	{
 		int ct=0;
 		DtList::iterator i=dtList_.begin(), e=dtList_.end();
 		for( ; i!=e; ++i, ++ct )
@@ -972,12 +978,13 @@ void ConfigManager::SetDocTypeByMenu( int pos, HMENU m )
 				::CheckMenuItem( m, ct, MF_BYPOSITION|MF_UNCHECKED);
 			}
 		}
-	//}
+	}
 }
 
 void ConfigManager::CheckMenu( HMENU m, int pos )
 {
-	/*if(app().isNewShell())
+#if !defined(TARGET_VER) || (defined(TARGET_VER) && TARGET_VER>310)
+	if(app().isNewShell())
 	{
 		MENUITEMINFO mi = { sizeof(MENUITEMINFO) };
 		mi.fMask  = MIIM_STATE;
@@ -990,7 +997,8 @@ void ConfigManager::CheckMenu( HMENU m, int pos )
 		}
 	}
 	else
-	{*/
+#endif
+	{
 		int ct=0;
 		DtList::iterator i=dtList_.begin(), e=dtList_.end();
 		for( ; i!=e; ++i, ++ct )
@@ -1004,6 +1012,6 @@ void ConfigManager::CheckMenu( HMENU m, int pos )
 				::CheckMenuItem( m, ct, MF_BYPOSITION|MF_UNCHECKED);
 			}
 		}
-	//}
+	}
 }
 
