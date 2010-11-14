294cc4fa6480ac1395430272db6aabc14ae44b43
Roy Tam <roytam@gmail.com>
2010-11-14 13:25:26 +0000

    - [cndos-up] check if apc_fetch function exist before use

--- cndos-up/progress.php
+++ cndos-up/progress.php
@@ -1,7 +1,7 @@
 <?php
-if(isset($_GET['progress_key'])) {
+if(isset($_GET['progress_key']) && function_exists('apc_fetch')) {
 	$status = apc_fetch('upload_'.$_GET['progress_key']);
 //	print_r($status);
-	printf('{"total":%s,"current":%s}',$status['total'],$status['current']);
+	printf('{"total":%u,"current":%u}',$status['total'],$status['current']);
 }
 ?>
