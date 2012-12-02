2c2f6848ad2b5c0049f5e884415323671c96aa31
roytam <roytam@d585c2e8-173e-3ebf-1cc4-9d1291936ed3>
2012-12-02 13:02:20 +0000

    [finddup_suite] - finddup.pl: add total possible dup count and current number and percentage for showing progress - dupselect.php: add comparing in 3-level directories

--- finddup_suite/finddup.pl
+++ finddup_suite/finddup.pl
@@ -5,17 +5,28 @@ use File::Find;
 use Digest::MD5;
  
 my %files;
+my $totalpossibledupfiles;
+my $possibledupfilescount;
  
 print STDERR "Searching files...\n";
 find(\&check_file, $ARGV[0] || ".");
  
 local $" = ", ";
+
+$possibledupfilescount = 0;
+$totalpossibledupfiles = 0;
+foreach my $size (keys %files) {
+  next unless @{$files{$size}} > 1;
+  $totalpossibledupfiles += $#{$files{$size}}+1;
+}
+
 foreach my $size (sort {$b <=> $a} keys %files) {
   next unless @{$files{$size}} > 1;
   my %md5;
   my $scnt = 1;
   foreach my $file (@{$files{$size}}) {
-    print STDERR "Hashing $file ($size ",$scnt++,"/",$#{$files{$size}}+1,")...\n";
+  	++$possibledupfilescount;
+    print STDERR "Hashing $file ($size ",$scnt++,"/",$#{$files{$size}}+1,") [",$possibledupfilescount,"/",$totalpossibledupfiles," ",(sprintf "%.2f", $possibledupfilescount*100/$totalpossibledupfiles),"%]...\n";
     open(FILE, $file) or next;
     binmode(FILE);
     push @{$md5{Digest::MD5->new->addfile(*FILE)->hexdigest}},$file;
