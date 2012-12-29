1fbf7c9e2e3ece2ca07a876bf6d6daf70496200a
roytam <roytam@d585c2e8-173e-3ebf-1cc4-9d1291936ed3>
2012-12-29 14:02:32 +0000

    [finddup_suite]  finddup.pl:  - move total count and group count to beginning of line  - add in-file switch(0/1) for showing total file count / total file size

--- finddup_suite/finddup.pl
+++ finddup_suite/finddup.pl
@@ -3,10 +3,15 @@
 use strict;
 use File::Find;
 use Digest::MD5;
- 
+
+my $printSize = 1;
+
+
 my %files;
 my $totalpossibledupfiles;
+my $totalpossibledupfilessize;
 my $possibledupfilescount;
+my $possibledupfilescountsize;
  
 print STDERR "Searching files...\n";
 find(\&check_file, $ARGV[0] || ".");
@@ -15,9 +20,12 @@ local $" = ", ";
 
 $possibledupfilescount = 0;
 $totalpossibledupfiles = 0;
+$totalpossibledupfilessize = 0;
+$possibledupfilescountsize = 0;
 foreach my $size (keys %files) {
   next unless @{$files{$size}} > 1;
   $totalpossibledupfiles += $#{$files{$size}}+1;
+  $totalpossibledupfilessize += $size*($#{$files{$size}}+1);
 }
 
 foreach my $size (sort {$b <=> $a} keys %files) {
@@ -25,8 +33,14 @@ foreach my $size (sort {$b <=> $a} keys %files) {
   my %md5;
   my $scnt = 1;
   foreach my $file (@{$files{$size}}) {
-  	++$possibledupfilescount;
-    print STDERR "Hashing $file ($size ",$scnt++,"/",$#{$files{$size}}+1,") [",$possibledupfilescount,"/",$totalpossibledupfiles," ",(sprintf "%.2f", $possibledupfilescount*100/$totalpossibledupfiles),"%]...\n";
+    ++$possibledupfilescount;
+    $possibledupfilescountsize+=$size;
+    if($printSize) {
+      print STDERR "[",properSize($possibledupfilescountsize),"/",properSize($totalpossibledupfilessize)," ",(sprintf "%.2f", $possibledupfilescountsize*100/$totalpossibledupfilessize),"%] ";
+    } else {
+      print STDERR "[$possibledupfilescount/$totalpossibledupfiles ",(sprintf "%.2f", $possibledupfilescount*100/$totalpossibledupfiles),"%] ";
+    }
+    print STDERR "($size ",$scnt++,"/",$#{$files{$size}}+1,") Hashing $file ...\n";
     open(FILE, $file) or next;
     binmode(FILE);
     push @{$md5{Digest::MD5->new->addfile(*FILE)->hexdigest}},$file;
@@ -45,3 +59,25 @@ sub check_file {
   -f && push @{$files{(stat(_))[7]}}, $File::Find::name;
 }
 
+sub properSize {
+ my $size = shift;
+ my $unit = shift || 3;
+ my $dp   = shift || 2;
+
+ my @units = ("","KB","MB","GB","TB","PB","EB");
+ my $i = 0;
+ my $out = "";
+
+ for($i = 0; $i < $unit && $size > 1024; $i++) {
+  $size /= 1024;
+ }
+
+ $out = sprintf("%.*f",$i?$dp:0,$size);
+
+ # add thousand separators
+ $out = reverse $out;
+ $out =~ s/(\d{3})(?=\d)(?!\d*\.)/$1,/g;
+ $out = reverse $out;
+
+ $out.$units[$i];
+}
