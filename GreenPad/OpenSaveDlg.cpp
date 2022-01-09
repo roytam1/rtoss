773edc27c82f588a8603ad64d7d6527527cb12d6
roytam1 <roytam@gmail.com>
2022-01-09 12:12:53 +0800

    [GreenPad] comdlg: ignore ErrCode == 0 as well

--- GreenPad/OpenSaveDlg.cpp
+++ GreenPad/OpenSaveDlg.cpp
@@ -426,7 +426,7 @@ bool OpenFileDlg::DoModal( HWND wnd, const TCHAR* fltr, const TCHAR* fnm )
 	if(ret != TRUE) {
 		ErrCode = ::GetLastError();
 
-		if(ErrCode == ERROR_NO_MORE_FILES) {
+		if(!ErrCode || ErrCode == ERROR_NO_MORE_FILES) {
 			// user pressed Cancel button
 		} else if((ErrCode == ERROR_INVALID_PARAMETER || ErrCode == ERROR_CALL_NOT_IMPLEMENTED || ErrCode == ERROR_INVALID_ACCEL_HANDLE) && ((ofn.Flags & OFN_EXPLORER) == OFN_EXPLORER)) {
 			// maybe Common Dialog DLL doesn't like OFN_EXPLORER, try again without it
@@ -558,7 +558,7 @@ bool SaveFileDlg::DoModal( HWND wnd, const TCHAR* fltr, const TCHAR* fnm )
 	if(ret != TRUE) {
 		ErrCode = ::GetLastError();
 
-		if(ErrCode == ERROR_NO_MORE_FILES) {
+		if(!ErrCode || ErrCode == ERROR_NO_MORE_FILES) {
 			// user pressed Cancel button
 		} else if((ErrCode == ERROR_INVALID_PARAMETER || ErrCode == ERROR_CALL_NOT_IMPLEMENTED) && ((ofn.Flags & OFN_EXPLORER) == OFN_EXPLORER)) {
 			// maybe Common Dialog DLL doesn't like OFN_EXPLORER, try again without it
