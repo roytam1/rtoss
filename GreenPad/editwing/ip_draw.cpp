2fab9ff18eb004887f5ba4389a8483f6faf25953
roytam1 <roytam@gmail.com>
2023-01-06 11:04:09 +0800

    [GreenPad] follow-up rev 2a0570fd

--- GreenPad/editwing/ip_draw.cpp
+++ GreenPad/editwing/ip_draw.cpp
@@ -273,10 +273,14 @@ inline void Painter::CharOut( unicode ch, int x, int y )
 #ifdef WIN32S
 	DWORD dwNum;
 	char psText[16]; // Buffer for a SINGLE multibyte character
-	if(dwNum = WideCharToMultiByte(CP_ACP,0, &ch,1, psText,countof(psText), NULL,NULL))
+	if(!(dwNum = WideCharToMultiByte(CP_ACP,0, &ch,1, psText,countof(psText), NULL,NULL)))
 	{
-		::TextOutA( dc_, x, y, psText, dwNum );
+		// Last fallback: truncate each widechar to a byte.
+		// Needed for Win32s beta 61.
+		dwNum = 1;
+		psText[0] = (char)ch;
 	}
+		::TextOutA( dc_, x, y, psText, dwNum );
 #else
 	::TextOutW( dc_, x, y, &ch, 1 );
 #endif
