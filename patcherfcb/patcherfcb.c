87947d77f3d1bdbf4969ef0e31ed27892bb5724b
roytam1 <roytam@gmail.com>
2024-12-29 23:43:20 +0800

    [patcherfcb] multiple changes: - store line length in variable and check it before comparing - add usage routine - add code to parse arguments - add ways to supply patch from stdin - add reverse patch function

--- patcherfcb/patcherfcb.c
+++ patcherfcb/patcherfcb.c
@@ -15,20 +15,35 @@
 #include <string.h>
 #include <stdlib.h>
 
+/* Reverse flag */
+int rev;
+
+/* Help flag */
+int help;
+
+/* Error counter */
+int err = 0;
+
 int apply_patch(FILE *fpin, FILE *fppat, int reverse, int* ap) {
 	char line[256];
-	int err = 0;
-	int i;
-	int offset, from, to, was;
+	int myerr = 0;
+	int i, l;
+	int offset, from, to, was, tmp;
 
 	while (fgets(line, 255, fppat)) {
-		for (i = 0; (i<strlen(line)) && (line[i] == ' '); i++)
+		l = strlen(line);
+		for (i = 0; (i<l) && (line[i] == ' '); i++)
 			;
-		if((line[i] != ';') && (strncmp(&line[i], "Comparing files", 15))) {
+		if((l >= 15) && (line[i] != ';') && (strncmp(&line[i], "Comparing files", 15))) {
 			if(!strncmp(&line[i + 8], ":", 1) && !strncmp(&line[i + 9], " ", 1) && !strncmp(&line[i + 12], " ", 1)) {
 				offset = strtol(&line[i], NULL, 16);
 				from = strtol(&line[i + 10], NULL, 16);
 				to = strtol(&line[i + 13], NULL, 16);
+				if(reverse) {
+					tmp = from;
+					from = to;
+					to = tmp;
+				}
 				if (!fseek(fpin, offset, SEEK_SET)) {
 					was = fgetc(fpin);
 					if (was != EOF) {
@@ -39,17 +54,17 @@ int apply_patch(FILE *fpin, FILE *fppat, int reverse, int* ap) {
 						if (was != from) {
 							printf("Offset $%04.4X was $%02.2X, should have been $%02.2X\n",
 								offset, was, from);
-							err = 1;
+							myerr = 1;
 						}
 					}
 					else {
 						printf("Offset $%04.4X EOF\n", offset);
-						err = 2;
+						myerr = 2;
 					}
 				}
 				else {
 					printf("Offset $%04.4X seek error\n", offset);
-					err = 3;
+					myerr = 3;
 				}
 			}
 			else {
@@ -57,45 +72,120 @@ int apply_patch(FILE *fpin, FILE *fppat, int reverse, int* ap) {
 			}
 		}
 	}
-	return err;
+	return myerr;
 }
 
-void main(int argc, char *argv[]) {
-	FILE *fpin, *fppat;
-	int err = 0;
+void usage(char* myname)
+{
+	fprintf(stderr, "%s\n", myname);
+	fputs("Patches files using output from DOS fc/b command.\n\n",
+		  stderr);
+
+	if (err)
+	{
+		fputs("Invalid parameter\n\n", stderr);
+	}
+
+	fprintf(stderr, "    %s [options] [drive:1][path1]file [drive2:][path2]patchfile\n",
+	        myname);
+
+	fprintf(stderr, "    %s [options] [drive:1][path1]file - < [drive2:][path2]patchfile\n",
+	        myname);
+
+	fprintf(stderr, "    other-command | %s [options] [drive:][path]file -\n", myname);
+	fputs("\n	Options:\n", stderr);
+	fputs("    /R   Reverse patch\n", stderr);
+	fputs("    /?   Help\n", stderr);
+	fputs("\n    Notice: \"-\" is indicator for stdin\n", stderr);
+}
+
+int main(int argc, char **argv) {
+	FILE *fpin = 0, *fppat = 0;
+	char *infname = 0, *patfname = 0;
+	char *cp;
+	char **orig_argv = argv;
+
+	int argnow = 0;
 	int ap = 1;
 	int rflag = 0;
 
-	if (argc == 3) {
-		if (fpin = fopen(argv[1], "rb+")) {
-			if (fppat = fopen(argv[2], "rb")) {
-				err = apply_patch(fpin, fppat, rflag, &ap);
-				fclose(fppat);
-				switch (err) {
-				case 0:
-					printf("File patched OK\n");
+	while (--argc) {
+		++argnow;
+		if (*(cp = *++argv) == '/') {
+			switch (cp[1]) {
+				case 'R':
+				case 'r':
+					rflag = 1;
 					break;
-				case 1:
-					if (ap == 0)
-						printf("Incorrect file for this patch!\n");
-					else
-						printf("File already patched\n");
+
+				case '?':
+				case 'h':
+				case 'H':
+					help = 1;
 					break;
-				case 2:
-					printf("End of file error\n");
-					break;
-				case 3:
-					printf("Seek error\n");
-					break;
+
+				default:
+					err++;
+			}
+		} else {
+			// try openning it, if it can be fopen()ed, record its name
+			FILE* fptest;
+			if (!infname) {
+				if((fptest = fopen(*argv, "rb+")) != NULL) {
+					infname = *argv;
+					fpin = fptest;
+				} else {
+					fprintf(stderr, "%s: input file not found\n", orig_argv[0]);
+					exit(3);
+				}
+			} else if (!patfname) { // patch file
+				if((fptest = fopen(*argv, "rb")) != NULL) {
+					patfname = *argv;
+					fppat = fptest;
+				} else {
+					if(!strcmp(*argv, "-")) {
+					patfname = *argv;
+					fppat = (FILE*)stdin;
+					} else {
+						fprintf(stderr, "%s: patch file not found\n", orig_argv[0]);
+						exit(3);
+					}
 				}
 			}
-			else
-				printf("Can't open patch file\n");
-			fclose(fpin);
-		}
-		else
-			printf("Can't open file\n");
+	   }
 	}
-	else
-		printf("Usage: `%s <file> <patch file>'\n", argv[0]);
+	if (!help && (!infname || !patfname))
+		err++;
+
+	if (err || help)
+	{
+		usage(orig_argv[0]);
+		exit(1);
+	}
+
+	err = apply_patch(fpin, fppat, rflag, &ap);
+	if (!strcmp(patfname, "-"))
+		fclose(fppat);
+
+	switch (err) {
+		case 0:
+			printf("File patched OK\n");
+			break;
+		case 1:
+			if (ap == 0)
+				printf("Incorrect file for this patch!\n");
+			else
+				printf("File already patched\n");
+			break;
+		case 2:
+			printf("End of file error\n");
+			break;
+		case 3:
+			printf("Seek error\n");
+			break;
+	}
+
+	fclose(fpin);
+
+	return err;
 }
