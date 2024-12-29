8beb74bd339139ab24cb50e1737099544a3e4a9c
roytam1 <roytam@gmail.com>
2024-12-29 22:24:08 +0800

    [patcherfcb] move main patching routine to a function, add rflag (WIP)

--- patcherfcb/patcherfcb.c
+++ patcherfcb/patcherfcb.c
@@ -15,53 +15,61 @@
 #include <string.h>
 #include <stdlib.h>
 
-void main(int argc, char *argv[]) {
-	FILE *fpin, *fppat;
+int apply_patch(FILE *fpin, FILE *fppat, int reverse, int* ap) {
 	char line[256];
+	int err = 0;
 	int i;
 	int offset, from, to, was;
+
+	while (fgets(line, 255, fppat)) {
+		for (i = 0; (i<strlen(line)) && (line[i] == ' '); i++)
+			;
+		if((line[i] != ';') && (strncmp(&line[i], "Comparing files", 15))) {
+			if(!strncmp(&line[i + 8], ":", 1) && !strncmp(&line[i + 9], " ", 1) && !strncmp(&line[i + 12], " ", 1)) {
+				offset = strtol(&line[i], NULL, 16);
+				from = strtol(&line[i + 10], NULL, 16);
+				to = strtol(&line[i + 13], NULL, 16);
+				if (!fseek(fpin, offset, SEEK_SET)) {
+					was = fgetc(fpin);
+					if (was != EOF) {
+						fseek(fpin, offset, SEEK_SET);
+						fputc(to, fpin);
+						if (was != to)
+							ap = 0;
+						if (was != from) {
+							printf("Offset $%04.4X was $%02.2X, should have been $%02.2X\n",
+								offset, was, from);
+							err = 1;
+						}
+					}
+					else {
+						printf("Offset $%04.4X EOF\n", offset);
+						err = 2;
+					}
+				}
+				else {
+					printf("Offset $%04.4X seek error\n", offset);
+					err = 3;
+				}
+			}
+			else {
+				printf("invalid line skipped '%s'\n", line);
+			}
+		}
+	}
+	return err;
+}
+
+void main(int argc, char *argv[]) {
+	FILE *fpin, *fppat;
 	int err = 0;
 	int ap = 1;
+	int rflag = 0;
 
 	if (argc == 3) {
 		if (fpin = fopen(argv[1], "rb+")) {
 			if (fppat = fopen(argv[2], "rb")) {
-				while (fgets(line, 255, fppat)) {
-					for (i = 0; (i<strlen(line)) && (line[i] == ' '); i++)
-						;
-					if((line[i] != ';') && (strncmp(&line[i], "Comparing files", 15))) {
-						if(!strncmp(&line[i + 8], ":", 1) && !strncmp(&line[i + 9], " ", 1) && !strncmp(&line[i + 12], " ", 1)) {
-							offset = strtol(&line[i], NULL, 16);
-							from = strtol(&line[i + 10], NULL, 16);
-							to = strtol(&line[i + 13], NULL, 16);
-							if (!fseek(fpin, offset, SEEK_SET)) {
-								was = fgetc(fpin);
-								if (was != EOF) {
-									fseek(fpin, offset, SEEK_SET);
-									fputc(to, fpin);
-									if (was != to)
-										ap = 0;
-									if (was != from) {
-										printf("Offset $%04.4X was $%02.2X, should have been $%02.2X\n",
-											offset, was, from);
-										err = 1;
-									}
-								}
-								else {
-									printf("Offset $%04.4X EOF\n", offset);
-									err = 2;
-								}
-							}
-							else {
-								printf("Offset $%04.4X seek error\n", offset);
-								err = 3;
-							}
-						}
-						else {
-							printf("invalid line skipped '%s'\n", line);
-						}
-					}
-				}
+				err = apply_patch(fpin, fppat, rflag, &ap);
 				fclose(fppat);
 				switch (err) {
 				case 0:
