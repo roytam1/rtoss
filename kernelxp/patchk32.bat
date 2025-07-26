fad2e104c6abecbe562781a31562541883269df0
roytam1 <roytam@gmail.com>
2025-07-26 10:35:32 +0800

    [kernelxp] patchk32: handle paths that have space, and patch api-ms-win-core-* DLLs in different way

--- kernelxp/patchk32.bat
+++ kernelxp/patchk32.bat
@@ -1,4 +1,7 @@
 @echo off
 for /r %%i in (*.dll;*.exe) do (
- if not %%~nxi.==kernelxp.dll. %~dp0gsar -sKERNEL32.DLL -rKERNELXP.DLL -i -o %%i
+ if not %%~nxi.==kernelxp.dll. "%~dp0gsar" -sKERNEL32.DLL -rKERNELXP.DLL -i -o "%%~i"
+)
+for /r %%i in (api-ms-win-core-*.dll) do (
+ "%~dp0gsar" -skernel32. -rkernelxp. -i -o "%%~i"
 )
