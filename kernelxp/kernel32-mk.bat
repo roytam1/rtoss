8cdcd93b01379a7875ad749e075a0f0dad5b028c
roytam1 <roytam@gmail.com>
2025-08-03 00:17:37 +0800

    [kernelxp] mk: partly revert path setting, add x64 specified files

--- kernelxp/kernel32-mk.bat
+++ kernelxp/kernel32-mk.bat
@@ -1,1 +1,1 @@
-lib /def:%~dp0kernel32.def /out:kernel32.lib
+lib /def:kernel32.def /out:kernel32.lib
--- kernelxp/kernelxp-mk.bat
+++ kernelxp/kernelxp-mk.bat
@@ -6,4 +6,4 @@ cl /c /O1 %~dp0kernelxp_stubs.cpp
 %~dp0gsar -sHeapAlloc@12 -rHeapAlloc:x00:x00:x00 -o kernelxp_stubs.obj
 %~dp0gsar -sGetProcessHeap@0 -rGetProcessHeap:x00:x00 -o kernelxp_stubs.obj
 %~dp0gsar -sGetSystemInfo@4 -rGetSystemInfo:x00:x00 -o kernelxp_stubs.obj
-link /dll /def:%~dp0kernelxp.def /out:kernelxp.dll /entry:SetDllDirectoryW /force kernelxp_stubs.obj .\kernel32.lib
+link /dll /def:kernelxp.def /out:kernelxp.dll /entry:SetDllDirectoryW /force kernelxp_stubs.obj .\kernel32.lib
